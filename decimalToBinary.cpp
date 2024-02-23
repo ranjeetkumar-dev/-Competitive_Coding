db87da1c57a63547b37d73e0afd9dba35bb01552
Ranjeet Kumar <ranjeetkumar.dev2@gmail.com>
2024-02-23 19:47:25 +0530

    deciToBin

--- decimalToBinary.cpp
+++ decimalToBinary.cpp
@@ -2,22 +2,46 @@
 #include <math.h>
 using namespace std;
 
-int deciToBin(int n)
-{
+// int deciToBin(int n)
+// {
 
-    int ans = 0;
+//     int ans = 0;
+//     int i = 0;
+
+//     while (n != 0)
+//     {
+//         int bit = n & 1;
+
+//         ans = (bit * pow(10, i)) + ans;
+//         n = n >> 1;
+//         i++;
+//     }
+
+//     return ans;
+// }
+
+void decToBin(int n)
+{
+    if (n == 0)
+    {
+        cout << 0;
+        return;
+    }
+
+    int bin[32];
     int i = 0;
 
-    while (n != 0)
+    while (n > 0)
     {
-        int bit = n & 1;
-
-        ans = (bit * pow(10, i)) + ans;
-        n = n >> 1;
+        bin[i] = n % 2;
+        n /= 2;
         i++;
     }
 
-    return ans;
+    for (int j = i - 1; j >= 0; j--)
+    {
+        cout << bin[j];
+    }
 }
 int main()
 {
@@ -25,8 +49,8 @@ int main()
     cout << "Enter decimal no:";
     cin >> n;
 
-    cout<<deciToBin(n);
+    // cout<<deciToBin(n);
+    decToBin(n);
 
     return 0;
 }
-
